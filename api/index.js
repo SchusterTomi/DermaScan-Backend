505d77bb28fe76160069ac0555d5ab49bf9a1a5a
schustertomi <163531821+SchusterTomi@users.noreply.github.com>
2025-06-12 15:30:15 -0300

    Adaptando el index para vercel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
-const pool = require('../db');
-const imagenRoutes = require('../routes/imagen.routes'); // <--- esta línea debe ir arriba, antes de usarla
+const pool = require('./db');
+const imagenRoutes = require('./routes/imagen.routes');
 
 const app = express();
 
@@ -11,22 +11,16 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas
-app.use("/api/imagen", imagenRoutes); // <--- ya podés usarla
+app.use("/api/imagen", imagenRoutes);
 
-module.exports = app;
-
-// Ruta de prueba
-app.get('/test-db', async (req, res) => {
+app.get('/api/test-db', async (req, res) => {
   try {
     const result = await pool.query('SELECT NOW()');
     res.json({ status: 'Conectado!', hora: result.rows[0].now });
   } catch (err) {
-    console.error('Error en /test-db:', err);
+    console.error('Error en /api/test-db:', err);
     res.status(500).json({ status: 'Error al conectar con la base de datos' });
   }
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+module.exports = app;
