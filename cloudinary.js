4318fcad1c6221383bdef926d57da2462b92cd01
schustertomi <163531821+SchusterTomi@users.noreply.github.com>
2025-11-20 14:59:51 -0300

    chaucha (error) cloudinary logs

--- cloudinary.js
+++ cloudinary.js
@@ -1,12 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config(); // Importa las variables de entorno
 
-console.log("Cloudinary config:", {
-  name: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_API_SECRET ? "CARGADO" : "NO CARGADO"
-});
-    
+// ❌ NO logueamos las credenciales
+// console.log("Cloudinary config:", { ... });
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
